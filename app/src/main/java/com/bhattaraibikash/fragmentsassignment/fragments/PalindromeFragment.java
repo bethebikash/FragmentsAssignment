b5eff91e3542d1aa603876f5b690c793db590b64
bethebikash <bhattaraibikash55@gmail.com>
2019-12-10 22:20:53 +0545

    validation in palindrome checking.

--- app/src/main/java/com/bhattaraibikash/fragmentsassignment/fragments/PalindromeFragment.java
+++ app/src/main/java/com/bhattaraibikash/fragmentsassignment/fragments/PalindromeFragment.java
@@ -2,15 +2,15 @@ package com.bhattaraibikash.fragmentsassignment.fragments;
 
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
 
 import com.bhattaraibikash.fragmentsassignment.R;
 
@@ -21,6 +21,7 @@ public class PalindromeFragment extends Fragment implements View.OnClickListener
 
 
     private Button btnCheckPalindrome;
+    private TextView tvPalindrome;
     private EditText etNumberPal;
 
     public PalindromeFragment() {
@@ -35,6 +36,7 @@ public class PalindromeFragment extends Fragment implements View.OnClickListener
         View view = inflater.inflate(R.layout.fragment_palindrome, container, false);
 
         etNumberPal = view.findViewById(R.id.etNumberPal);
+        tvPalindrome = view.findViewById(R.id.tvPalindrome);
         btnCheckPalindrome = view.findViewById(R.id.btnCheckPalindrome);
 
         btnCheckPalindrome.setOnClickListener(this);
@@ -44,26 +46,28 @@ public class PalindromeFragment extends Fragment implements View.OnClickListener
 
     @Override
     public void onClick(View v) {
-
-        int num = Integer.parseInt(etNumberPal.getText().toString());
-
-        int i = 0;
-        int r = 0;
-
-        int initialNum = num;
-
-        for (i = 0; i <= num; i++) {
-            r = r * 10;
-            r = r + num % 10;
-            num = num / 10;
-            i = 0;
-        }
-
-        if (r == initialNum) {
-            Toast.makeText(getActivity(),"The number is a Palindrome number", Toast.LENGTH_SHORT).show();
+        if (etNumberPal.getText().toString().isEmpty()) {
+            etNumberPal.setError("enter the number");
         } else {
-            Toast.makeText(getActivity(),"The number is not a Palindrome number", Toast.LENGTH_SHORT).show();
+            int num = Integer.parseInt(etNumberPal.getText().toString());
+
+            int i;
+            int r = 0;
+
+            int initialNum = num;
+
+            for (i = 0; i <= num; i++) {
+                r = r * 10;
+                r = r + num % 10;
+                num = num / 10;
+                i = 0;
+            }
+
+            if (r == initialNum) {
+                tvPalindrome.setText(initialNum+" is a Palindrome Number");
+            } else {
+                tvPalindrome.setText(initialNum+" is not a Palindrome Number");
+            }
         }
     }
-
 }
