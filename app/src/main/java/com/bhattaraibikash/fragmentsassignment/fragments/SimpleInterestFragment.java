8bb513e114110041cbfd97fc0515090ad73ea70d
bethebikash <bhattaraibikash55@gmail.com>
2019-12-10 22:07:12 +0545

    validation in Simple Interest Calculation.

--- app/src/main/java/com/bhattaraibikash/fragmentsassignment/fragments/SimpleInterestFragment.java
+++ app/src/main/java/com/bhattaraibikash/fragmentsassignment/fragments/SimpleInterestFragment.java
@@ -2,15 +2,14 @@ package com.bhattaraibikash.fragmentsassignment.fragments;
 
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
 
 import com.bhattaraibikash.fragmentsassignment.R;
 
@@ -20,6 +19,7 @@ import com.bhattaraibikash.fragmentsassignment.R;
 public class SimpleInterestFragment extends Fragment implements View.OnClickListener {
 
     private Button btnCalculateSI;
+    private TextView tvSI;
     private EditText etAmount, etRate, etTime;
 
 
@@ -36,6 +36,7 @@ public class SimpleInterestFragment extends Fragment implements View.OnClickList
         etAmount = view.findViewById(R.id.etAmount);
         etTime = view.findViewById(R.id.etTime);
         etRate = view.findViewById(R.id.etRate);
+        tvSI = view.findViewById(R.id.tvSI);
         btnCalculateSI = view.findViewById(R.id.btnCalculateSI);
 
         btnCalculateSI.setOnClickListener(this);
@@ -44,12 +45,20 @@ public class SimpleInterestFragment extends Fragment implements View.OnClickList
 
     @Override
     public void onClick(View v) {
-        float p = Float.parseFloat(etAmount.getText().toString());
-        float t = Float.parseFloat(etTime.getText().toString());
-        float r = Float.parseFloat(etRate.getText().toString());
+        if (etAmount.getText().toString().isEmpty()) {
+            etAmount.setError("enter the amount!");
+        } else if (etTime.getText().toString().isEmpty()) {
+            etTime.setError("enter the time!");
+        } else if (etRate.getText().toString().isEmpty()) {
+            etRate.setError("enter the rate!");
+        } else {
+            float p = Float.parseFloat(etAmount.getText().toString());
+            float t = Float.parseFloat(etTime.getText().toString());
+            float r = Float.parseFloat(etRate.getText().toString());
 
-        float si = (p*t*r)/100;
+            float si = (p * t * r) / 100;
 
-        Toast.makeText(getActivity(),"Simple Interest is : " + si, Toast.LENGTH_SHORT).show();
+            tvSI.setText("Simple Interest is : " + si);
+        }
     }
 }
