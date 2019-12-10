00a5409218f3fa785ab7fcf24ca16ba10edc8b6c
bethebikash <bhattaraibikash55@gmail.com>
2019-12-10 09:51:24 +0545

    Six fragments added for six individual tasks.

---
+++ app/src/main/java/com/bhattaraibikash/fragmentsassignment/fragments/AreaFragment.java
@@ -0,0 +1,32 @@
+package com.bhattaraibikash.fragmentsassignment.fragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bhattaraibikash.fragmentsassignment.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AreaFragment extends Fragment {
+
+
+    public AreaFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_area, container, false);
+    }
+
+}
