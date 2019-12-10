f80e119d05ee52cc5d65d111619380d9b1dd33ea
bethebikash <bhattaraibikash55@gmail.com>
2019-12-10 15:52:10 +0545

    Simple Interest calculation logic added in the SimpleInterestFragment.

--- app/src/main/java/com/bhattaraibikash/fragmentsassignment/fragments/SimpleInterestFragment.java
+++ app/src/main/java/com/bhattaraibikash/fragmentsassignment/fragments/SimpleInterestFragment.java
@@ -8,13 +8,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.bhattaraibikash.fragmentsassignment.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SimpleInterestFragment extends Fragment {
+public class SimpleInterestFragment extends Fragment implements View.OnClickListener {
+
+    private Button btnCalculateSI;
+    private EditText etAmount, etRate, etTime;
 
 
     public SimpleInterestFragment() {
@@ -26,7 +32,24 @@ public class SimpleInterestFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_simple_interest, container, false);
+        View view = inflater.inflate(R.layout.fragment_simple_interest, container, false);
+        etAmount = view.findViewById(R.id.etAmount);
+        etTime = view.findViewById(R.id.etTime);
+        etRate = view.findViewById(R.id.etRate);
+        btnCalculateSI = view.findViewById(R.id.btnCalculateSI);
+
+        btnCalculateSI.setOnClickListener(this);
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        float p = Float.parseFloat(etAmount.getText().toString());
+        float t = Float.parseFloat(etTime.getText().toString());
+        float r = Float.parseFloat(etRate.getText().toString());
+
+        float si = (p*t*r)/100;
+
+        Toast.makeText(getActivity(),"Simple Interest is : " + si, Toast.LENGTH_SHORT).show();
+    }
 }
