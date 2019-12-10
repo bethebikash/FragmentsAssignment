257ed2aad0cc494e94c3b7ae984c633756255131
bethebikash <bhattaraibikash55@gmail.com>
2019-12-10 20:00:14 +0545

    working on checking automorphic number.

--- app/src/main/java/com/bhattaraibikash/fragmentsassignment/fragments/AutomorphicFragment.java
+++ app/src/main/java/com/bhattaraibikash/fragmentsassignment/fragments/AutomorphicFragment.java
@@ -2,20 +2,24 @@ package com.bhattaraibikash.fragmentsassignment.fragments;
 
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
 
 import com.bhattaraibikash.fragmentsassignment.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AutomorphicFragment extends Fragment {
+public class AutomorphicFragment extends Fragment implements View.OnClickListener {
 
+    private EditText etNumberAu;
+    private Button btnCheckAutomorphic;
 
     public AutomorphicFragment() {
         // Required empty public constructor
@@ -26,7 +30,28 @@ public class AutomorphicFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_automorphic, container, false);
+        View view = inflater.inflate(R.layout.fragment_automorphic, container, false);
+        etNumberAu = view.findViewById(R.id.etNumberAu);
+        btnCheckAutomorphic = view.findViewById(R.id.btnCheckAutomorphic);
+
+        btnCheckAutomorphic.setOnClickListener(this);
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+
+        int num = Integer.parseInt(etNumberAu.getText().toString());
+        int sqNum = num * num;
+
+        String strNum = Integer.toString(num);
+        String strSqNumber = Integer.toString(sqNum);
+
+        if (strSqNumber.endsWith(strNum)) {
+            Toast.makeText(getActivity(), "It an Automorphic Number", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getActivity(), "Not an Automorphic Number", Toast.LENGTH_SHORT).show();
+        }
+
+    }
 }
